c27bc504a374e054218eebed794fe4087f35acb2
gotbbt14BrittanyGates <lexicon_carnage.0t@icloud.com>
2025-04-09 19:48:24 +0200

    minor fixes

---
+++ rollup.config.mjs
@@ -0,0 +1,28 @@
+// rollup.config.mjs
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+  ],
+  external: ["react", "@tanstack/react-query", "wagmi", "viem"],
+};
