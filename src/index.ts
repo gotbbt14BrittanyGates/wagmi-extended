1c62c335d9fdc19944aa009ca061b5b15e1422b1
gotbbt14BrittanyGates <lexicon_carnage.0t@icloud.com>
2025-07-21 11:04:46 +0200

    added useHandleTransactionMutationX to export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 export * from "./hooks/mutations/useContractWriteX.js";
 export * from "./hooks/mutations/useSendTransactionX.js";
 export * from "./hooks/mutations/useERC20ApproveX.js";
+export * from "./hooks/mutations/useHandleTransactionMutationX.js";
 
 /* ------------- */
 /*     Queries   */
